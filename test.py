e9a1001182ebb488c2beea25a7dcdba493600705
Bolang-Lonson <bbeinyoh12@gmail.com>
2023-12-31 08:45:30 +0100

    testing the if __name__ is __main__ to run as script

--- test.py
+++ test.py
@@ -1,4 +1,28 @@
-document = (['singing', 'quizzing'],)
+def isanagram(word1, word2):
+    word1, word2 = word1.upper(), word2.upper()
+    word1, word2 = word1.replace('', ' '), word2.replace('', ' ')
+    word1, word2 = word1.split(), word2.split()
+    word1.sort()
+    word2.sort()
+    return word1 == word2
 
-document[0].append('poetry')
-print(document)
+
+def main():
+    print('ANAGRAM'.center(27, '-'))
+    while True:
+        first = input('First word\n')
+        secnd = input('Second word\n')
+        if isanagram(first, secnd):
+            print('Anagrams')
+        else:
+            print('Not anagrams')
+
+        loop = int(input('1) Back\n2) Close\n'))
+        match loop:
+            case 1:
+                continue
+            case 2:
+                break
+
+if __name__ == '__main__':
+    main()
