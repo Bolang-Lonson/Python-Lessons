2500ce0fa7ee7bb4cf072f6aca41c908220b8679
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-04-05 20:47:14 +0100

    seeing decorators with arguments making them more flexible and useful

---
+++ PA-OOP/OOP Advanced/10_Decorators2.py
@@ -0,0 +1,57 @@
+"""Decorators, which should be universal, must support any function, regardless of the number and type of arguments passed. In such a situation, we can use the *args and **kwargs concepts. We can also employ a closure technique to persist arguments."""
+
+def simple_decorator(own_function):
+
+    def internal_wrapper(*args, **kwargs):
+        print('"{}" was called with the following arguments'.format(own_function.__name__))
+        print('\t{}\n\t{}\n'.format(args, kwargs))
+        own_function(*args, **kwargs)
+        print('Decorator is still operating')
+
+    return internal_wrapper
+
+
+@simple_decorator
+def combiner(*args, **kwargs):
+    print("\tHello from the decorated function; received arguments:", args, kwargs)
+
+combiner('a', 'b', exec='yes')
+
+"""Arguments passed to the decorated function are available to the decorator, so the decorator can print them. This is a simple example, as the arguments were just printed, but not processed further.
+A nested function (internal_wrapper) could reference an object (own_function) in its enclosing scope thanks to the closure."""
+
+def warehouse_decorator(material):
+    def wrapper(our_function):
+        def internal_wrapper(*args):
+            print('<strong>*</strong> Wrapping items from {} with {}'.format(our_function.__name__, material))
+            our_function(*args)
+            print()
+        return internal_wrapper
+    return wrapper
+
+
+@warehouse_decorator('kraft')
+def pack_books(*args):
+    print("We'll pack books:", args)
+
+
+@warehouse_decorator('foil')
+def pack_toys(*args):
+    print("We'll pack toys:", args)
+
+
+@warehouse_decorator('cardboard')
+def pack_fruits(*args):
+    print("We'll pack fruits:", args)
+
+
+pack_books('Alice in Wonderland', 'Winnie the Pooh')
+pack_toys('doll', 'car')
+pack_fruits('plum', 'pear')
+
+"""The pack_books function will be executed as follows:
+
+the warehouse_decorator('kraft') function will return the wrapper function;
+the returned wrapper function will take the function it is supposed to decorate as an argument;
+the wrapper function will return the internal_wrapper function, which adds new functionality (material display) and runs the decorated function.
+"""
