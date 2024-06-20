0238bb79b6a14d336f0fdb5b98cb5defeaf1d4b4
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-06-20 10:49:59 +0100

    final version with html version checker

--- htmlParser.py
+++ htmlParser.py
@@ -2,6 +2,13 @@ from html.parser import HTMLParser
 
 class MyHtmlParser(HTMLParser):
     isTitle: bool = False
+    login_conditions = {
+        'has_pass_field': False,
+        'has_submit': False,
+        'has_btn': False,
+        'has_post_form': False,
+        'has_login_button': False
+    }
     header_count: dict[str: int] = {
         'h1': 0,
         'h2': 0,
@@ -37,21 +44,19 @@ class MyHtmlParser(HTMLParser):
             for attr in attrs:
                 if attr[0] == 'type':
                     input_type = attr[1] 
-                    self.has_pass_field = True if input_type == 'password' else False
-                    self.has_submit = True if input_type == 'submit' else False
-                    self.has_btn = True if input_type == 'button' else False
+                    if input_type == 'password': self.login_conditions['has_pass_field'] = True
+                    if input_type == 'submit': self.login_conditions['has_submit'] = True 
+                    if input_type == 'button': self.login_conditions['has_btn'] = True 
 
                 # If we have a submit input we can read the value attribute for human language hints
-                if attr[0] == 'value' and (self.has_submit or self.has_btn):
+                if attr[0] == 'value' and (self.login_conditions['has_submit'] or self.login_conditions['has_btn']):
                     # using common human language button text for login buttons
-                    self.has_login_button = True if attr[1].lower() in ['login', 'log-in', 'log in', 'signin', 'sign-in', 'sign in'] else False
-                else:
-                    self.has_login_button = False
+                    if attr[1].lower() in ['login', 'log-in', 'log in', 'signin', 'sign-in', 'sign in']: self.login_conditions['has_login_button'] = True
 
         # getting the form's method
         if tag == 'form':
             for attr in attrs:
-                self.has_post_form = True if (attr[0] == 'method' and attr[1].lower() == 'post') else False
+                if (attr[0] == 'method' and attr[1].lower() == 'post'): self.login_conditions['has_post_form'] = True 
 
 
     def handle_data(self, data: str) -> None:
@@ -75,8 +80,18 @@ parser = MyHtmlParser()
 
 # parser.feed(html_content)
 doc = open('index.html', 'rt')
-html_content = doc.read()  #   reading from html file
+html_content = doc.read().strip()  #   reading from html file
 doc.close()
+
+# looking out for <!DOCTYPE html>
+if html_content.startswith('<!DOCTYPE html>'):
+    print('Document is html:5')
+elif html_content.startswith('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"'):
+    print('Document is html:4.01')
+elif html_content.startswith('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.'):
+    print('Document is xhtml:1')
+else:
+    print('Unknown html version')
 
 parser.feed(html_content)
 parser.close()
@@ -87,5 +102,7 @@ external = [url for url in parser.anchor_hrefs if url.startswith('http')]
 internal = [url for url in parser.anchor_hrefs if url not in external]
 print('External links: ', external)
 print('Internal links: ', internal)
-print(f'Has password input: {parser.has_pass_field}')
-print(f'Has submit button: {parser.has_submit}. Is login? {parser.has_login_button}')
+# condition to conclude login page
+if parser.login_conditions['has_pass_field']:
+    if parser.login_conditions['has_login_button'] or parser.login_conditions['has_post_form']:
+        print('Probably a login page')
