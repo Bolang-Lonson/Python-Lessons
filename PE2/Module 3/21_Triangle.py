0ba375e154282bdc42df359454c5be7757547e0e
Bolang-Lonson <bbeinyoh12@gmail.com>
2025-08-07 10:11:14 +0100

    chronological numbering and few type annotation mods

--- PE2/Module 3/21_Triangle.py
+++ PE2/Module 3/21_Triangle.py
@@ -2,22 +2,22 @@ import math
 
 class Point:
     def __init__(self, x=0.0, y=0.0):
-        self.__x = x
-        self.__y = y
+        self.__x: float = x
+        self.__y: float = y
 
-    def getx(self):
+    def getx(self) -> float:
         return self.__x
 
-    def gety(self):
+    def gety(self) -> float:
         return self.__y
 
-    def distance_from_xy(self, x, y):
+    def distance_from_xy(self, x: float, y: float) -> float:
         dx = self.__x - x
         dy = self.__y - y
         dist = math.hypot(dx, dy)
         return dist
 
-    def distance_from_point(self, point):
+    def distance_from_point(self, point: 'Point') -> float:
         dx = self.__x - point.getx()
         dy = self.__y - point.gety()
         dist = math.hypot(dx, dy)
@@ -25,10 +25,10 @@ class Point:
 
 
 class Triangle:
-    def __init__(self, vertex1, vertex2, vertex3):
+    def __init__(self, vertex1: Point, vertex2: Point, vertex3: Point):
         self.__vertices = [vertex1, vertex2, vertex3]
 
-    def perimeter(self):
+    def perimeter(self) -> float:
         d12 = self.__vertices[0].distance_from_point(self.__vertices[1])
         d23 = self.__vertices[1].distance_from_point(self.__vertices[2])
         d13 = self.__vertices[0].distance_from_point(self.__vertices[2])
